dc7ae72f0f34e8e3d235085601520767c432a0a1
Ermolkin <stigmatervlad@gmail.com>
2017-11-24 17:35:21 +0300

    Create conversation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124125220) do
+ActiveRecord::Schema.define(version: 20171124143135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "conversations_users", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "conversation_id"
+    t.index ["conversation_id"], name: "index_conversations_users_on_conversation_id"
+    t.index ["user_id"], name: "index_conversations_users_on_user_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "text"
